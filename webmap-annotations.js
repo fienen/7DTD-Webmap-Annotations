cfc250c9aacdabc68e30b55b7319c8a763285b1c
Michael Fienen <fienen@gmail.com>
2015-02-23 11:00:47 -0600

    Resolves #8

--- webmap-annotations.js
+++ webmap-annotations.js
@@ -5,64 +5,66 @@
 // @version     0.1
 // ==/UserScript==
 
-// Loading confirmation
-console.log('Initializing 7DTD Webmap Annotations script...');
+$(document).ready( function() {
+	// Loading confirmation
+	console.log('Initializing 7DTD Webmap Annotations script...');
 
-// ===============================================================================================
-// Add Marker control
+	// ===============================================================================================
+	// Add Marker control
 
-L.Control.AddMarker = L.Control.extend({
-	/*
-	options: {
-		position: 'bottomleft'
-	},
-	*/
+	L.Control.AddMarker = L.Control.extend({
+		/*
+		options: {
+			position: 'bottomleft'
+		},
+		*/
 
-	onAdd: function (map) {
-		var name = 'control-addmarker',
-		    container = L.DomUtil.create('div', name + ' leaflet-bar');
+		onAdd: function (map) {
+			var name = 'control-addmarker',
+			    container = L.DomUtil.create('div', name + ' leaflet-bar');
 
-		this._map = map;
+			this._map = map;
 
-		this._reloadbutton = this._createButton(
-			"Add Marker", "Add Marker",
-			name + "-btn", container, this._reload, this);
+			this._reloadbutton = this._createButton(
+				"Add Marker", "Add Marker",
+				name + "-btn", container, this._reload, this);
 
-		return container;
-	},
+			return container;
+		},
 
-	onRemove: function (map) {
-	},
+		onRemove: function (map) {
+		},
 
-	_reload: function (e) {
-		tileTime = new Date().getTime();
-		tileLayer.redraw();
-		tileLayerMiniMap.redraw();
-	},
+		_reload: function (e) {
+			tileTime = new Date().getTime();
+			tileLayer.redraw();
+			tileLayerMiniMap.redraw();
+		},
 
-	_createButton: function (html, title, className, container, fn, context) {
-		var link = L.DomUtil.create('a', className, container);
-		link.innerHTML = html;
-		link.href = '#';
-		link.title = title;
+		_createButton: function (html, title, className, container, fn, context) {
+			var link = L.DomUtil.create('a', className, container);
+			link.innerHTML = html;
+			link.href = '#';
+			link.title = title;
 
-		var stop = L.DomEvent.stopPropagation;
+			var stop = L.DomEvent.stopPropagation;
 
-		L.DomEvent
-		    .on(link, 'click', stop)
-		    .on(link, 'mousedown', stop)
-		    .on(link, 'dblclick', stop)
-		    .on(link, 'click', L.DomEvent.preventDefault)
-		    .on(link, 'click', fn, context)
-		    .on(link, 'click', this._refocusOnMap, context);
+			L.DomEvent
+			    .on(link, 'click', stop)
+			    .on(link, 'mousedown', stop)
+			    .on(link, 'dblclick', stop)
+			    .on(link, 'click', L.DomEvent.preventDefault)
+			    .on(link, 'click', fn, context)
+			    .on(link, 'click', this._refocusOnMap, context);
 
-		return link;
-	}
+			return link;
+		}
 
+	});
+
+	new L.Control.AddMarker({
+	}).addTo(map);
+
+	// Loading complete confirmation
+	console.log('7DTD Webmap Annotations initialization complete.');
 });
-
-new L.Control.AddMarker({
-}).addTo(map);
-
-// Loading complete confirmation
-console.log('7DTD Webmap Annotations initialization complete.');
