1d90d55e6ea8666afe8caf510d9fdb8c28e5d69e
Michael Fienen <fienen@gmail.com>
2015-02-23 10:48:43 -0600

    Initial function for #1

--- webmap-annotations.js
+++ webmap-annotations.js
@@ -4,3 +4,57 @@
 // @include     http://*/static/index.html
 // @version     0.1
 // ==/UserScript==
+
+// ===============================================================================================
+// Add Marker control
+
+L.Control.AddMarker = L.Control.extend({
+	options: {
+		position: 'bottomleft'
+	},
+
+	onAdd: function (map) {
+		var name = 'control-addmarker',
+		    container = L.DomUtil.create('div', name + ' leaflet-bar');
+
+		this._map = map;
+
+		this._reloadbutton = this._createButton(
+			"Add Marker", "Add Marker",
+			name + "-btn", container, this._reload, this);
+
+		return container;
+	},
+
+	onRemove: function (map) {
+	},
+
+	_reload: function (e) {
+		tileTime = new Date().getTime();
+		tileLayer.redraw();
+		tileLayerMiniMap.redraw();
+	},
+
+	_createButton: function (html, title, className, container, fn, context) {
+		var link = L.DomUtil.create('a', className, container);
+		link.innerHTML = html;
+		link.href = '#';
+		link.title = title;
+
+		var stop = L.DomEvent.stopPropagation;
+
+		L.DomEvent
+		    .on(link, 'click', stop)
+		    .on(link, 'mousedown', stop)
+		    .on(link, 'dblclick', stop)
+		    .on(link, 'click', L.DomEvent.preventDefault)
+		    .on(link, 'click', fn, context)
+		    .on(link, 'click', this._refocusOnMap, context);
+
+		return link;
+	}
+
+});
+
+new L.Control.AddMarker({
+}).addTo(map);
